524fea804707bf1f9fa02d572cbbe7200945c59e
Bush, Nicholas <Nicholas.Bush@seattlechildrens.org>
2024-03-22 18:17:42 -0700

    Pipeline runs ephysqc on the longest files

--- preprocess/pipeline.py
+++ preprocess/pipeline.py
@@ -1,19 +1,44 @@
 import subprocess
 import click
 import logging
-from pathlib import Path
-from ibllib.ephys.ephysqc import EphysQC
+from ibllib.ephys.ephysqc import extract_rmsmap
+import spikeglx
+import sys
 logging.basicConfig()
 _log = logging.getLogger('PIPELINE')
 _log.setLevel(logging.INFO)
 
+
 def run_ephys_qc_session(session_path):
-    session_path = Path(session_path)
-    probe_paths = session_path.glob('raw_ephys_data/probe*')
-    for probe_path in probe_paths:
-        qc = EphysQC(session_path,use_alyx=False)
-        qc.probe_path = probe_path
-        qc.run()
+    """Get the RMS maps only on the longest recording of a session
+
+    Args:
+        session_path (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    efiles = spikeglx.glob_ephys_files(session_path)
+    max_duration = 0 
+    qc_files = []
+    longest_ap = None
+    longest_lf = None
+    for efile in efiles:
+        if efile.get('ap') and efile.ap.exists():
+            SR = spikeglx.Reader(efile.ap)
+            if SR.ns>max_duration:
+                max_duration = SR.ns
+                longest_ap = efile['ap']
+                if efile.get('lf'):
+                    longest_lf = efile['lf']
+    if longest_ap:
+        SR = spikeglx.Reader(longest_ap)
+        qc_files.extend(extract_rmsmap(SR, out_folder=None, overwrite=False))
+    if longest_lf:
+        SR = spikeglx.Reader(longest_lf)
+        qc_files.extend(extract_rmsmap(SR, out_folder=None, overwrite=False))
+    return qc_files
+
 
 @click.command()
 @click.argument('session_path', type=click.Path(exists=True))
