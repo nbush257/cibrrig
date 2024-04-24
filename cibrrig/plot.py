7c76fae138560a252e333747cb64261b9c226545
nbush <nbush257@gmail.com>
2024-04-24 09:49:54 -0700

    expand plot_laser capability. added convinience function to clean a linear phase axis.

--- cibrrig/plot.py
+++ cibrrig/plot.py
@@ -3,17 +3,71 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.colors as mcolors
 from .utils.utils import weighted_histogram
+from one.alf.io import AlfBunch
+import seaborn as sns
+
+laser_colors ={
+473:'#00b7ff',
+565:'#d2ff00',
+653:'#ff0000'
+}
+
+def plot_laser(laser_in,**kwargs):
+    if isinstance(laser_in,AlfBunch):
+        _plot_laser_alf(laser_in,**kwargs)
+    else:
+        _plot_laser_intervals(laser_in,**kwargs)
+
+def _plot_laser_alf(laser_in,**kwargs):
+    intervals = laser_in.intervals
+    if 'amplitudesMilliwatts' in laser_in.keys():
+        amplitudes = laser_in.amplitudesMilliwatts
+        amp_label='mw'
+    elif 'amplitudesVolts' in laser_in.keys():
+        amplitudes = laser_in.amplitudesVolts
+        amp_label='command volts'
+    else:
+        amplitudes = None
 
 
-def plot_laser(intervals,amplitudes,ax=None):
+    _plot_laser_intervals(intervals,amplitudes,amp_label=amp_label,**kwargs)
+
+
+def _plot_laser_intervals(intervals,amplitudes=None,ax=None,mode='shade',amp_label='',wavelength=473,alpha=0.2,**kwargs):
     if ax is None:
         f = plt.figure()
         ax = f.add_subplot(111)
-    # interleave zeros for the offsets
-    new_amps = np.vstack([np.zeros_like(amplitudes),amplitudes]).T.ravel()
-    plt.step(intervals.ravel(),new_amps)
-    
+    try:
+        iter(alpha)
+        alpha_list = True
+    except:
+        alpha_list = False
 
+    if mode=='shade':
+        for ii,stim in enumerate(intervals):
+            aa = alpha[ii] if alpha_list else alpha
+            ax.axvspan(stim[0],stim[1],color = laser_colors[wavelength],alpha = aa,**kwargs)
+    elif mode=='bar':
+        yy = ax.get_ylim()[1]
+        yy = np.ones_like(intervals[:,0])*yy
+        ax.hlines(yy,intervals[:,0],intervals[:,1],color = laser_colors[wavelength],**kwargs)
+    elif mode=='vline':
+        y0 = ax.get_ylim()[0]
+        y0 = np.ones_like(intervals[:,0])*y0
+
+        y1 = ax.get_ylim()[1]
+        y1 = np.ones_like(intervals[:,0])*y1
+        ax.vlines(intervals[:,0],y0,y1,color=laser_colors[wavelength],**kwargs)
+    else:
+    # interleave zeros for the offsets
+        if amplitudes is None:
+            new_amps = np.vstack([np.zeros_like(intervals[:,0]),np.ones_like(intervals[:,0])]).T.ravel()
+        else:
+            new_amps = np.vstack([np.zeros_like(amplitudes),amplitudes]).T.ravel()
+        ax.step(intervals.ravel(),new_amps,color=laser_colors[wavelength],**kwargs)
+        ax.set_ylabel(amp_label)
+        
+    
 
 # TODO: This works, but needs to be expanded to work for 3D and to be more complete.
 def plot_projection_line(X,cvar,dims=[0,1],cmap='viridis'):
@@ -100,7 +154,7 @@ def plot_2D_projection(X,dims=[0,1],cvar=None,ax=None,title='',
     else:
         vmin = vmin or np.min(cvar)
         vmax = vmax or np.max(cvar)
-        p = ax.scatter(X[:,dims[0]],X[:,dims[1]],color=cvar,s=s,cmap=cmap,alpha=alpha,
+        p = ax.scatter(X[:,dims[0]],X[:,dims[1]],c=cvar,s=s,cmap=cmap,alpha=alpha,
                        vmin=vmin,vmax=vmax)
         if plot_colorbar:
             cax = f.add_axes([0.25, 0.85, 0.5, 0.02])
@@ -126,6 +180,11 @@ def clean_polar_axis(ax):
     ax.set_yticks([ax.get_yticks()[-1]])
     ax.set_xticks([0,np.pi/2,np.pi,np.pi*3/2])
     ax.set_xticklabels(['0','$\\frac{\pi}{2}$','$\pi$','$\\frac{-\pi}{2}$'])
+
+def clean_linear_radial_axis(ax):
+    ax.set_xticks([-np.pi,-np.pi/2,0,np.pi/2,np.pi])
+    ax.set_xticklabels(['$-\pi$','$\\frac{-\pi}{2}$','0','$\\frac{\pi}{2}$','$\pi$'])
+    sns.despine(trim=True)
 
 
 def plot_polar_average(x,y,t,ax=None,t0=None,tf=None,color='k',bins=50,multi='sem',alpha=0.3,**plot_kwargs):
@@ -164,6 +223,8 @@ def plot_polar_average(x,y,t,ax=None,t0=None,tf=None,color='k',bins=50,multi='se
     if ax is None:
         f = plt.figure()
         ax = f.add_subplot(projection='polar')
+    else:
+        f = None
 
     if multi=='sem':
         lb = m - np.nanstd(y_polar_out,0)/np.sqrt(y_polar_out.shape[0])
@@ -184,6 +245,7 @@ def plot_polar_average(x,y,t,ax=None,t0=None,tf=None,color='k',bins=50,multi='se
                 c = color[ii]
             ax.plot(phase_bins,y_polar,color=c,**plot_kwargs)
     clean_polar_axis(ax)
+    return(f,ax,y_polar_out,phase_bins)
 
 
 
