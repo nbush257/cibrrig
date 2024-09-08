d48f00fe58d1b7a01a174d7b7fed6d18ada4880a
nbush257 <nbush257@gmail.com>
2024-09-08 00:02:25 -0700

    Update Sphinx documentation configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx.ext.duration',
+   'sphinx.ext.doctest',
+   'sphinx.ext.autodoc',
+   'sphinx.ext.autosummary',
 ]
 
 templates_path = ['_templates']
