c2d270f1a76862fa66714c1f3c5d192b55693367
nbush257 <nbush257@gmail.com>
2025-10-03 12:26:34 -0700

    rename intermediate spikeinterface folders so they are not hidden

--- cibrrig/sorting/spikeinterface_ks4.py
+++ cibrrig/sorting/spikeinterface_ks4.py
@@ -627,12 +627,12 @@ def run_probe(probe_src, probe_local, testing=False, skip_remove_opto=False):
 
 
     # Set paths
-    si_path = probe_local.joinpath(".si")
+    si_path = probe_local.joinpath("si")
     # Temporary paths that will not be coming with us?
-    preproc_path = si_path.joinpath(".preprocessed")
-    sort_path = si_path.joinpath(".sort")
-    motion_path = si_path.joinpath(".motion")
-    analyzer_path = si_path.joinpath(".analyzer")
+    preproc_path = si_path.joinpath("preprocessed")
+    sort_path = si_path.joinpath("sort")
+    motion_path = si_path.joinpath("motion")
+    analyzer_path = si_path.joinpath("analyzer")
     exported_alf_path = si_path.joinpath("kilosort4")
     probe_local.mkdir(parents=True, exist_ok=True)
     #
